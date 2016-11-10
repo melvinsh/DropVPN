08af6c0aa0ab301c41bcd5878fb49661d1eb11e3
Melvin Lammerts <hi@melvin.sh>
2016-11-10 17:40:03 +0100

    Add key generation to droplet creation

--- dropvpn.rb
+++ dropvpn.rb
@@ -1,10 +1,21 @@
 require 'pry'
+require 'sshkey'
 require 'droplet_kit'
 
 token = ENV['DO_TOKEN']
 client = DropletKit::Client.new(access_token: token)
 
-binding.pry
+key = SSHKey.generate(type: 'RSA', bits: 2048)
+ssh_key = DropletKit::SSHKey.new(public_key: key.ssh_public_key, name: 'DropVPN')
 
-droplet = DropletKit::Droplet.new(name: 'DropVPN-Netherlands', image: 'ubuntu-16-04-x64', size: '512mb', region: 'ams3')
-client.droplets.create(droplet)
+created_key = client.ssh_keys.create(ssh_key)
+
+droplet = DropletKit::Droplet.new(
+  name: 'DropVPN-Netherlands',
+  image: 'ubuntu-16-04-x64',
+  size: '512mb',
+  region: 'ams3',
+  ssh_keys: [created_key.id]
+)
+
+created = client.droplets.create(droplet)
